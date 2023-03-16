e25adc2c0f3cf9bd504b0d641c0c89f221d3e732
Mattéo Christophe <matteo.christophe@opquast.com>
2023-03-16 15:39:28 +0100

    feat: product creation on the go

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,67 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+const stripe = require('stripe')('sk_test_51MhVWdK5m5JuNCOY8BauuEGBtt8E86uLxCmqIw4rsFry4Idm6R5jfGT2uuqA6qISPQYbMSCIaBnLT5RwYmToTxOQ00FLOVipFB');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const YOUR_DOMAIN = 'http://localhost:3000';
+
+
+async function does_product_exist(name){
+  const product_list = await stripe.products.list();
+  const products = product_list.data
+
+  for (let index = 0; index < products.length; index++) {
+    const current_name = products[index].name;
+    if (name == current_name) {
+      return [true, products[index].id]
+    }
+  }
+  return [false, null]
 }
-bootstrap();
+
+app.post('/create-checkout-session', async (req, res) => {
+  let name = req.body.name
+  let qty = req.body.qty
+  let base_price = req.body.base_price
+  let description = req.body.description
+
+  let [exist, id] = await does_product_exist(name)
+  console.log(exist, id)
+
+  if(exist){
+    const product = await stripe.products.retrieve(id);
+  } else {
+    const product = await stripe.products.create({
+      name: name,
+      images: [],
+      description: description,
+    });
+    id = product.id
+  }
+
+  const price = await stripe.prices.create({
+    unit_amount: base_price * 100,
+    currency: 'eur',
+    product: id,
+    tax_behavior: "exclusive",
+  });
+
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: price.id,
+        quantity: qty,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    automatic_tax: {enabled: true},
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Product {
+  name: string,
+  base_price: number,
+  description: string
 }
 
-export default App;
+const ProductDisplay = ({name, base_price, description} :Product) => (
+  <section>
+    <div className="product">
+      <img
+        src="https://i.imgur.com/EHyR2nP.png"
+        alt="The cover of Stubborn Attachments"
+      />
+      <div className="description">
+      <h3>{name}</h3>
+      <h5>{base_price}€</h5>
+      </div>
+    </div>
+    <form action="/create-checkout-session" method="POST">
+      <input type="number" name="qty" defaultValue={1} min="1" max="100"></input>
+      <input type="hidden" name="name" value={name}></input>
+      <input type="hidden" name="base_price" value={base_price}></input>
+      <input type="hidden" name="description" value={description}></input>
+      <button type="submit">Checkout</button>
+    </form>
+  </section>
+);
+
+const Message = ({ message }: any) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
+
+export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  return message ? (
+    <Message message={message} />
+  ) : (
+    <ProductDisplay name="dseggzerg" base_price={100} description="Bon produit miam"/>
+  );
+}
