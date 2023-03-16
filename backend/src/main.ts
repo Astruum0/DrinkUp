26d6e61f8a532046fc31bf44331bd2e7459e7bf2
Mattéo Christophe <matteo.christophe@opquast.com>
2023-03-16 16:29:43 +0100

    temp

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CocktailSchema } from './Schemas/cocktail.schema';
+import AdminBro from 'admin-bro';
+import AdminBroMongoose from '@admin-bro/mongoose';
+import AdminBroExpress from '@admin-bro/express';
+AdminBro.registerAdapter(AdminBroMongoose);
 
+const AdminBroOptions = {
+  resources: [CocktailSchema],
+};
+
+const adminBro = new AdminBro(AdminBroOptions);
+const router = AdminBroExpress.buildRouter(adminBro);
+
+app.use(adminBro.options.rootPath, router);
+app.listen(8000, function () {
+  console.log('Listening to Port 8000');
+});
+AdminBro.registerAdapter(AdminBroMongoose);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
