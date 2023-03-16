f52047b003edaa73c855ad256a760f923c770d3f
Astruum0 <arthurvella33@gmail.com>
2023-03-16 16:11:58 +0100

    upload image with simple route and body

--- backend/src/Controllers/cocktail.controller.ts
+++ backend/src/Controllers/cocktail.controller.ts
@@ -1,36 +1,64 @@
 import { randomUUID } from 'crypto';
 import { Cocktail } from './../Schemas/cocktail.schema';
 import { CreateCocktailDto } from './../Dto/create-cocktail.dto';
-import { Controller, Get, Post, Param, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Delete,
+  Body,
+  UseInterceptors,
+  UploadedFile
+} from '@nestjs/common';
 import { CocktailsService } from 'src/Services/cocktail.service';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import path = require('path');
+
+const storage = {
+  storage: diskStorage({
+    destination: './uploads/cocktails',
+    filename: (req, file, cb) => {
+      const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + randomUUID();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`)
+    }
+  })
+}
 
 @Controller('cocktails')
 export class CocktailsController {
   constructor(private readonly cocktailService: CocktailsService) {}
 
   @Post('/create')
-  create(@Body() createCocktailDto: CreateCocktailDto) {
-    const cocktail: Cocktail = {
-      id: randomUUID(),
-      name: createCocktailDto.name,
-      picture: createCocktailDto.picture,
-      ingredients: createCocktailDto.ingredients,
-      description: createCocktailDto.description,
-      ratingsNb: null,
-      rating: null,
-    };
-    try {
-      return this.cocktailService.create(cocktail);
-    } catch (e: unknown) {
-      return {
-        error:
-          typeof e === 'string'
-            ? e.toUpperCase()
-            : e instanceof Error
-            ? e.message
-            : 'Error',
-      };
-    }
+  @UseInterceptors(
+    FileInterceptor('file', storage))
+  create(@UploadedFile() file) {
+    console.log("hello",file);
+    return {"file": file.filename}
+    // const cocktail: Cocktail = {
+    //   id: randomUUID(),
+    //   name: createCocktailDto.name,
+    //   picture: file.filename,
+    //   ingredients: createCocktailDto.ingredients,
+    //   description: createCocktailDto.description,
+    //   ratingsNb: null,
+    //   rating: null,
+    // };
+    // try {
+    //   return this.cocktailService.create(cocktail);
+    // } catch (e: unknown) {
+    //   return {
+    //     error:
+    //       typeof e === 'string'
+    //         ? e.toUpperCase()
+    //         : e instanceof Error
+    //         ? e.message
+    //         : 'Error',
+    //   };
+    // }
   }
 
   @Get()
