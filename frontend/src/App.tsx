dbd5e9ac59b385152a0fdca86d2caf30e99b5beb
paul-lautier <paul.lautier64@gmail.Com>
2023-03-17 14:48:32 +0100

    add button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import './App.css'
-
-
+import Button from './components/Button/Button'
 function App() {
     return (
         <div className="App">
-
+            <Button text="cocktail 1" href="#" type="Primary" />
             <p>oui</p>
         </div>
     )
---
+++ frontend/src/types.ts
@@ -0,0 +1,7 @@
+export interface ButtonInterface {
+    text: string
+    type: string
+    href: string
+    filled?: boolean
+    icon?: JSX.Element
+}
