7909d2ff6354cffdd3f25f319bacb25b3cbbef93
paul-lautier <paul.lautier64@gmail.Com>
2023-03-17 14:14:47 +0100

    remove MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './App.css'
-import DenseAppBar from './components/Navbar/Navbar'
-import SwipeableTextMobileStepper from './components/Carousel/Carousel'
+
+
 function App() {
     return (
         <div className="App">
-            <DenseAppBar />
-            <SwipeableTextMobileStepper />
+
             <p>oui</p>
         </div>
     )
