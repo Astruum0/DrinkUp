c6df0591cf81a1caf3f9f7e293d5aed9e2fe1f14
Astruum0 <arthurvella33@gmail.com>
2023-03-23 16:12:23 +0100

    add backend route for basic auth

---
+++ backend/src/Modules/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from "@nestjs/mongoose"
+import { UsersController } from 'src/Controllers/users.controller';
+import { UserSchema } from 'src/Models/users.model';
+import { UsersService } from 'src/Services/users.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: "user", schema: UserSchema }])],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+export class UsersModule {}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,16 @@ import { CocktailsModule } from './Modules/cocktail.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { IngredientsModule } from './Modules/ingredient.module';
+import { UsersModule } from './Modules/users.module';
+import { AuthModule } from './Modules/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1/drinkup'),
     IngredientsModule,
     CocktailsModule,
+    UsersModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
