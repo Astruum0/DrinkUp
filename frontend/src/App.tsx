6a22ec11614bacb3dd600288bbc8d567a0077895
paul-lautier <paul.lautier64@gmail.Com>
2023-03-30 15:16:12 +0200

    push card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,51 @@
+import ArticleIcon from '@mui/icons-material/Send'
+import CameraAltIcon from '@mui/icons-material/Send'
+import SendIcon from '@mui/icons-material/Send'
 import './App.css'
-import Button from './components/Button/Button'
+
+import Card from './components/Card/Card'
+
 function App() {
-    return (
-        <div className="App">
-            <Button text="cocktail 1" href="#" type="Primary" />
-            <p>oui</p>
-        </div>
-    )
+
+  return (
+    <div className="App">
+      <section className="card-container">
+        <Card
+          body='Cocktail de boisson alcoolisé pour pa'
+          title='Negroni'
+          image='https://source.unsplash.com/random'
+          badge={{
+            text: "Nouveau Cocktail",
+            filled: false,
+          }}
+          indicator="New"
+          btn={{
+            text: "Voir plus",
+            href: '#',
+            type: 'primary',
+            filled: true,
+            icon: <ArticleIcon />
+          }} />
+        <Card
+          body='Slurpy cocktaily.'
+          title='Vodka Pomme'
+          image='https://source.unsplash.com/random/500X400'
+          badge={{
+            text: "Classique incontournable",
+            filled: false,
+          }}
+          indicator="New"
+          subtitle='Get your photo now'
+          btn={{
+            text: "Voir plus",
+            href: '#',
+            type: 'secondary',
+            filled: true,
+            icon: <CameraAltIcon />
+          }} />
+      </section>
+    </div>
+  )
 }
 
 export default App
