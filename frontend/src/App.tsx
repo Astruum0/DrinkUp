f540b14aaa866dcdff6c14a28bd5143aec5cee7a
paul-lautier <paul.lautier64@gmail.Com>
2023-04-05 16:18:59 +0200

    change route / navbar comment useless code (mattéo L)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,8 +15,8 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/search" element={<SearchWithIngredients />} />
                 <Route path="/new" element={<CocktailForm />} />
-                <Route path="/cocktail" element={<CocktailList/>}/>
-                <Route path="/cocktailDetail/:id" element={<CocktailDetail/>} />
+                <Route path="/cocktails" element={<CocktailList/>}/>
+                {/* <Route path="/cocktailDetail/:id" element={<CocktailDetail/>} /> */}
             </Routes>
         </div>
     )
